cef9896a52908dac1f4ed0341204ad8de42f45f3
rjalvarez <69989382+rjalvarez80@users.noreply.github.com>
2020-09-27 11:53:57 -0600

    Update main.c

--- main.c
+++ main.c
@@ -23,13 +23,15 @@ int main(void) {
   else if( (s < 130) && (d < 80) ) {
    printf("Elevated");
   }
-  else if( (s < 140) || (d < 90)) {  
-    printf("Stage 1 Hypertension");
-  }
-  else if( (s > 180) || (d > 120)) {        // If we put Stage 2 first the program wouldn't know when s > 180 because it would execute at s > 140 without trying the 180.
+    else if( (s > 180) || (d > 120)) {      /* We start from top to bottom because we are looking for s or d grater than a specific number. If the number is not big enought we
+                          go to a lower number to see if it fall on the new calification. This is made so we don't have to specified ranges of evaluation on every if statemnt.*/
     printf("Hypertensive Crysis");
   }
   else if( (s >= 140) || (d >= 90)) {       // Equal signs are added after > so we can avoid the possible error of s = 140 and d = 90.
     printf("Stage 2 Hypertension");
   }
+  else if( (s < 140) || (d < 90)) {  
+    printf("Stage 1 Hypertension");
+  }
+
 }
