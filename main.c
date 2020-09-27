52e051fb61aa563e8545bb1e3035b7428c663e00
rjalvarez <69989382+rjalvarez80@users.noreply.github.com>
2020-09-27 12:05:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 *   CODING CHALLENGE #2
 *   Filename: main.c
 *   This file measures a person's blood pressure readings and reports their category.
-*   Last modified by: Angel F. Garcia (9/19/2020)
+*   Last modified by: Robert Alvarez (9/27/2020)
 *   
 *   Check the // TODO comments to see what is missing and needs to be completed.
 */
@@ -15,7 +15,7 @@ int main(void) {
   scanf("%d %d", &s, &d);                   // Read Systolic and Diastolic pressure
 
   if(s < d) {
-    printf("Error");
+    printf("ERROR");                        // Error go first so we don't have to evaluate an impossible scenario risking to output a fall statement.
   }
   else if( (s < 120) && (d < 80) ) {
     printf("Normal");
@@ -23,8 +23,9 @@ int main(void) {
   else if( (s < 130) && (d < 80) ) {
    printf("Elevated");
   }
-    else if( (s >= 180) || (d >= 120)) {      /* We start from top to bottom because we are looking for s or d grater than a specific number. If the number is not big enought we
-                          go to a lower number to see if it fall on the new calification. This is made so we don't have to specified ranges of evaluation on every if statemnt.*/
+  else if( (s >= 180) || (d >= 120)) {      /* We start from top to bottom because we are looking for s or d grater than or queal than a specific number. If the number is not
+                                               big enought we go to a lower number to see if it fall on the next calification. 
+                                               This is made so we don't have to specified ranges of evaluation on every if-else statemnt.*/
     printf("Hypertensive Crysis");
   }
   else if( (s >= 140) || (d >= 90)) {       // Equal signs are added after > so we can avoid the possible error of s = 140 and d = 90.
